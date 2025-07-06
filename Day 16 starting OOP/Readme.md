88268492c8bd1d8988440e6ea08de4ea268c1f6e
Hexon <ayushmaurya21086@gmail.com>
2025-07-06 17:11:12 +0000

    docs: update README and learn OOP notebook; remove unused test.py

--- Day 16 starting OOP/Readme.md
+++ Day 16 starting OOP/Readme.md
@@ -1,1 +1,2 @@
 started oops from this day 16 
+now making the coffe machine code but using oops with these files only 
--- Day 16 starting OOP/test.py
+++
@@ -1,7 +0,0 @@
-from turtle import Turtle, Screen
-timmy = Turtle()
-print("hello world")
-
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
