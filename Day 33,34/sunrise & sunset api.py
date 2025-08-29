f1babb974ab5d8ea9293969d7578443bd16a2245
Hexon <ayushmaurya21086@gmail.com>
2025-08-29 17:33:00 +0000

    Add flashcard application with image assets and CSV data handling
    - Implemented main functionality for a flashcard app using Tkinter.
    - Added logic to load words from a CSV file and manage known words.
    - Included image assets for card front and back, as well as buttons for user interaction.
    - Created a canvas for displaying flashcards and configured the layout.
    - Added functionality to flip cards and track known words.

---
+++ Day 33,34/sunrise & sunset api.py
@@ -0,0 +1,29 @@
+import requests
+import datetime as dt
+
+
+time_now=dt.datetime.now()
+
+
+parameter={
+    'lat':20.593683,
+    'lng':78.962883,
+    'formatted':0,
+}
+
+
+
+response=requests.get(url="https://api.sunrise-sunset.org/json",params=parameter)
+
+response.raise_for_status()
+
+data=response.json()
+
+sunrise=data["results"]["sunrise"].split('T')[1].split(':')[0]
+
+sunset=data["results"]["sunset"].split('T')[1].split(':')[0]
+
+print(sunrise)
+print(sunset)
+
+print(time_now.hour)
